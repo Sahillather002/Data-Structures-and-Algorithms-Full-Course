7774d2c9906b10cbbe3dd9505a7611dbd6ffbaac
sahil lather <sahillather002@gmail.com>
2021-10-27 07:13:34 +0530

    lcm code

---
+++ Dsa Course/1)Mathematics/6_Lcm.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int lcm(int a, int b){
+    int res = max(a,b);
+    while(true){
+        if(res%a==0 && res%b==0){
+            return res;
+        }
+        res++;
+    }
+    return res;
+}
+
+int main(){
+    cout<<lcm(4,6);
+}
---
+++ Dsa Course/1)Mathematics/6_LcmOptimal.cpp
@@ -0,0 +1,18 @@
+// a*b = gcd(a,b) * lcm(a,b)
+
+#include<iostream>
+using namespace std;
+
+int gcd(int a, int b){
+    if(b==0)
+    return a%b;
+    return gcd(b,a%b);
+}
+
+int lcm(int a, int b){
+    return(a*b)/gcd(a,b);
+}
+
+int main(){
+    cout<<lcm(4,6);
+}
