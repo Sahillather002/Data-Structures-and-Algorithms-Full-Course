5d5b8e43ba66afd71542f9bc1b1e27131d3ee038
sahil lather <sahillather002@gmail.com>
2021-10-27 07:26:41 +0530

    Check Prime Codes

---
+++ Dsa Course/1)Mathematics/7_CheckPrime.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+bool prime(int n){
+    if(n==1)
+    return false;
+    for(int i=2;i<n;i++)
+    if(n%i==0)
+    return false;
+    return true;
+}
+
+int main(){
+    cout<<prime(10);
+}
---
+++ Dsa Course/1)Mathematics/7_CheckPrimeOptimal.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+bool prime(int n)
+{
+    if (n == 1)
+        return false;
+    for (int i = 2; i*i < n; i++)
+        if (n % i == 0)
+            return false;
+    return true;
+}
+
+int main()
+{
+    cout << prime(10);
+}
