4ba0e486cf48c65f9c74332faea0a33683525379
sahil lather <sahillather002@gmail.com>
2021-10-27 07:40:30 +0530

    Prime Factors Code

---
+++ Dsa Course/1)Mathematics/8_PrimeFactors.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+/*bool isPrime(int n)
+{
+    if (n == 1)
+        return false;
+    if (n == 2 || n == 3)
+        return true;
+    if (n % 2 == 0 || n % 3 == 0)
+        return false;
+    for (int i = 5; i * i <= n; i = i + 6)
+        if (n % i == 0 || n % (i + 2) == 0)
+            return false;
+    return true;
+}
+*/
+void factor(int n){
+    for(int i=2;i<n;i++){
+        if(isPrime(i)){
+            int x =i;
+            while(n%x==0){
+                printf(i);
+                x=x*i;
+            }
+        }
+    }
+}
+
+int main(){
+    cout<<factor(10);
+}
---
+++ Dsa Course/1)Mathematics/8_PrimeFactorsOptimal.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int factor(int n){
+    if(n<=1)
+    return;
+    for(int i=2;i*i<=n;i++){
+        while(n%i==0){
+            printf(i);
+            n=n/i;
+        }
+    }
+    if(n>1)
+    return n;
+}
+
+int main(){
+    cout<<factor(10);
+}
