c9902b3d903c2d19e7a4a374975b1b8aef14fd0e
sahil lather <sahillather002@gmail.com>
2021-12-01 15:16:12 +0530

    first code

--- Dsa Course/2)Bit Magic/1.cpp
+++ Dsa Course/2)Bit Magic/1.cpp
@@ -5,4 +5,15 @@ int main(){
     cout<<(x&y)<<"\n"; // bitwise and
     cout<<(x|y)<<"\n"; // bitwise or 
     cout<<(x^y)<<"\n"; // bitwise xor
+    
+    cout<<(x<<1)<<endl;
+    cout<<(x<<2)<<endl;
+    cout<<(x>>1)<<endl;
+    cout<<(x>>2)<<endl;
+
+    unsigned int a = 1;
+    cout<<(~a)<<endl;
+    int b = 1;
+    cout<<(~b)<<endl;
+
 }
