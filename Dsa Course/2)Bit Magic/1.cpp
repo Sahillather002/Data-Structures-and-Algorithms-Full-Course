642a3daac21521a4a3cf8d81622eab74cf9561cf
sahil lather <sahillather002@gmail.com>
2021-12-01 14:54:35 +0530

    bit magic intro

---
+++ Dsa Course/2)Bit Magic/1.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x = 3 , y = 6;
+    cout<<(x&y)<<"\n"; // bitwise and
+    cout<<(x|y)<<"\n"; // bitwise or 
+    cout<<(x^y)<<"\n"; // bitwise xor
+}
---
+++ Dsa Course/Python Practice/3_1_exercise-print-function-task.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+Print function.
+"""
+
+print("one\ttwo\tthree\tfour")
