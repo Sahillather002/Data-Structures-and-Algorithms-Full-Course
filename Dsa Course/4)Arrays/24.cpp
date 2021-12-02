8d68bef56bcf9a13ec9dad8c80c3eaad831d8ed5
sahil lather <sahillather002@gmail.com>
2021-12-03 00:29:26 +0530

    effective sol max sub array

--- Dsa Course/4)Arrays/24.cpp
+++ Dsa Course/4)Arrays/24.cpp
@@ -21,8 +21,24 @@ int fs(int arr[] , int n)
 
 }
 
+// efficient solution
+// time complexity - O(n)
+
+int ss(int arr[],int n)
+{
+    int res = arr[0];
+    int maxend = arr[0];
+
+    for(int i=1;i<n;i++)
+    {
+        maxend = max(maxend+arr[i],arr[i]);
+        res = max(res , maxend);
+    }
+    return res;
+}
 int main()
 {
     int arr[]={1,-2,3,-1,2};
     cout<<fs(arr,5);
+    cout<<ss(arr,5);
 }
