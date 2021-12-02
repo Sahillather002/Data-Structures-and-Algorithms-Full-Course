40a4a06442d24fb46eed0b47590dec55f5ad0a51
sahil lather <sahillather002@gmail.com>
2021-12-03 00:22:01 +0530

    max sum of subarray - naive solution having complexity O(n^2)

--- Dsa Course/4)Arrays/24.cpp
+++ Dsa Course/4)Arrays/24.cpp
@@ -3,5 +3,26 @@
 using namespace std;
 
 // naive solution
+// time complexity -- O(n^2)
 
-int fs(int arr[])
+int fs(int arr[] , int n)
+{
+    int res = arr[0];
+    for(int i=0;i<n;i++)
+    {
+        int curr =0;
+        for(int j=i;j<n;j++)
+        {
+            curr = curr + arr[i];
+            res = max(res,curr);
+        }
+    }
+    return res;
+
+}
+
+int main()
+{
+    int arr[]={1,-2,3,-1,2};
+    cout<<fs(arr,5);
+}
