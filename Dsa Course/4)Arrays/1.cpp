ba2f67588753c9c439be0c2a3b13523d913f78f1
sahil lather <sahillather002@gmail.com>
2021-12-02 15:29:59 +0530

    intro array

---
+++ Dsa Course/4)Arrays/1.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int arr[]={1,2,3,4,5};
+    cout<<arr;
+    cout<<arr[3];
+}
+
+/*
+Stack Allocated--
+INT ARR[100]
+INT ARR[N]
+
+Heap Allocated arrary--
+int *arr=new int[n]
+
+while in java arrays are always allocated in heap
+
+*/
