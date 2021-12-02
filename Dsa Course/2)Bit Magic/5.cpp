0e341875d35baf192bbffde3feb88ff0cad1eb63
sahil lather <sahillather002@gmail.com>
2021-12-02 15:24:23 +0530

    start new

---
+++ Dsa Course/2)Bit Magic/5.cpp
@@ -0,0 +1,36 @@
+// finding only odd occurring number
+#include <iostream>
+using namespace std;
+
+// solution 1
+/*
+int main()
+{
+    arr[] = {4, 3, 4, 4, 4, 5, 5};
+    for (int i = 0; i < n; i++)
+    {
+        int count = 0;
+        for (int j = 0; j < n; j++)
+            if (arr[j] == arr[i])
+                count++;
+        if ((count % 2) ! = 0)
+            printf(arr[i]);
+    }
+}
+*/
+
+// second solution using xor operator
+
+int findOdd(int arr[], int n)
+{
+    int res = 0;
+    for (int i = 0; i < n; i++)
+        res = res ^ arr[i];
+    return res;
+}
+
+main()
+{
+    int arr[] = {4, 3, 4, 4, 4, 5, 5};
+    cout << findOdd(arr[7], 7);
+}
