943d16ce9babbb9956a9ed659eee37d5f8912939
sahil lather <sahillather002@gmail.com>
2021-12-02 22:51:51 +0530

    effective solution for trapping rain water problem

---
+++ Dsa Course/4)Arrays/20.cpp
@@ -0,0 +1,29 @@
+// trappin rain water problem
+#include <iostream>
+using namespace std;
+
+// effective solution
+
+int ss(int arr[], int n)
+{
+    int res = 0;
+    int lmax[n], rmax[n];
+    lmax[0] = arr[0];
+    for (int i = 1; i < n; i++)
+        lmax[i] = max(arr[i], lmax[i - 1]);
+
+    rmax[n - 1] = arr[n - 1];
+    for (int i = n - 2; i >= 0; i--)
+        rmax[i] = max(arr[i], rmax[i + 1]);
+
+    for (int i = 1; i < n - 1; i++)
+        res = res + (min(lmax[i], rmax[i]) - arr[i]);
+
+    return res;
+}
+
+int main()
+{
+    int arr[] = {3, 0, 1, 2, 5};
+    cout << ss(arr, 5);
+}
