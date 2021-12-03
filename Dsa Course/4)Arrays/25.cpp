f047818965b6323eb6dd3c5277904766c5efe867
sahil lather <sahillather002@gmail.com>
2021-12-03 11:34:06 +0530

    cadane's algo - max even-odd length subarray

--- Dsa Course/4)Arrays/25.cpp
+++ Dsa Course/4)Arrays/25.cpp
@@ -22,8 +22,26 @@ int fs(int arr[],int n)
     return res;
 }
 
+// efficient solution -- cadane's algo
+int ss(int arr[],int n)
+{
+    int res = 1;
+    int curr = 1;
+    for(int i=1;i<n;i++)
+    {
+        if((arr[i]%2==0 && arr[i-1]%2 !=0)
+        || (arr[i]%2 !=0 && arr[i-1]==0))
+        {
+            curr++;
+            res = max(res,curr);
+        }
+        else curr = 1;
+    }
+    return res;
+}
 int main()
 {
     int arr[]={5,10,20,6,3,8};
     cout<<fs(arr,6);
+    cout<<ss(arr,6);
 }
