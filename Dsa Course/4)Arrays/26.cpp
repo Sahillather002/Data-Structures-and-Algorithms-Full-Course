09ce26ce33eceac76a6b5fa31d33af186447c497
sahil lather <sahillather002@gmail.com>
2021-12-03 15:20:19 +0530

    second sol. cicular sum of subarray with time complexity O(n)

--- Dsa Course/4)Arrays/26.cpp
+++ Dsa Course/4)Arrays/26.cpp
@@ -22,8 +22,44 @@ int fs(int arr[],int n)
     return res;
 }
 
+//solution 2 - using kadane's algo
+//complexity -- O(n)
+
+int normalmaxsum(int arr[],int n)
+{
+    int res = arr[0], maxend = arr[0];
+    for(int i = 1;i<n;i++)
+    {
+        maxend = max(arr[i],maxend+arr[i]);
+        res = max(res , maxend);
+    }
+    return res;
+    
+}
+
+int ss(int arr[],int n)
+{
+    int max_normal = normalmaxsum(arr,n); // normal sum
+    if(max_normal<0)
+    return max_normal;
+
+    int arr_sum = 0;
+    for(int i=0;i<n;i++)
+    {
+        arr_sum += arr[i];
+        arr[i] = -arr[i];
+
+    }
+    int max_circular = arr_sum + normalmaxsum(arr,n);
+
+    return max(max_normal , max_circular);
+
+}
+
+
 int main()
 {
     int arr[] = {5,-2,3,4};
     cout<<fs(arr,4);
+    cout<<ss(arr,4);
 }
