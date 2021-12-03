904906f05f6496f1c567de2bc623f70592b7ddd6
sahil lather <sahillather002@gmail.com>
2021-12-03 15:52:33 +0530

    majority element code

--- Dsa Course/4)Arrays/27.cpp
+++ Dsa Course/4)Arrays/27.cpp
@@ -19,8 +19,33 @@ int fs(int arr[],int n)
     return -1;
 }
 
+// O(n) , effective solution
+
+int ss(int arr[],int n)
+{
+    int res = 0,count = 1;
+    for(int i=1;i<n;i++)
+    {
+        if(arr[res]==arr[i])
+        count++;
+        else
+        count--;
+        if(count ==0)
+        res=i,count=1;
+    }
+
+    count = 0;
+    for(int i=0;i<n;i++)
+    if(arr[res]==arr[i])
+    count ++;
+    if(count<=n/2)
+    res = -1;
+
+    return res;
+}
 int main()
 {
     int arr[]={8,7,6,8,6,6,6,6};
     cout<<fs(arr,8);
+    cout<<ss(arr,8);
 }
