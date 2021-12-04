05c9e4ed4db2bcb5473328064ad9e9a0533172d9
sahil lather <sahillather002@gmail.com>
2021-12-04 16:36:00 +0530

    sol using recursive binary search

--- Dsa Course/5)Searching/3.cpp
+++ Dsa Course/5)Searching/3.cpp
@@ -13,9 +13,30 @@ int fs(int arr[],int n,int x)
     return -1;
 }
 
+// second sol.
+// using recursive binary search
+int ss(int arr[],int low,int high,int x)
+{
+    if(low>high)
+    return -1;
+    int mid = (low+high)/2;
+    if(x>arr[mid])
+    return ss(arr,mid+1,high,x);
+    else 
+    if(x<arr[mid])
+    return ss(arr, low,mid-1,x);
+    else
+    {
+        if(mid == 0 || arr[mid-1]!=arr[mid])
+        return mid;
+        else
+        return ss(arr, low , mid-1,x);
+    }
+}
 
 int main()
 {
     int arr[]={5,10,10,15,15};
     cout<<fs(arr,5,5);
+    cout<<ss(arr,0,4,5);
 }
