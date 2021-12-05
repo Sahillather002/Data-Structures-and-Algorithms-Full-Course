bf966bf5c65e5091e97e76b007c132b2b84c7835
sahil lather <sahillather002@gmail.com>
2021-12-05 10:13:52 +0530

    iterative binary search to find the last occurence of element in an array

--- Dsa Course/5)Searching/4.cpp
+++ Dsa Course/5)Searching/4.cpp
@@ -1,29 +1,50 @@
 // index of last occurence
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int fs(int arr[],int low,int high , int x,int n)
+int fs(int arr[], int low, int high, int x, int n)
 {
-    if(low>high)
-    return -1;
-    int mid = (low+high)/2;
-    if(arr[mid]>x)
-    return fs(arr,low,mid-1,x,n);
-    else if(arr[mid]<x)
-    return fs(arr,mid+1,high,x,n);
+    if (low > high)
+        return -1;
+    int mid = (low + high) / 2;
+    if (arr[mid] > x)
+        return fs(arr, low, mid - 1, x, n);
+    else if (arr[mid] < x)
+        return fs(arr, mid + 1, high, x, n);
     else
     {
-        if(mid == n-1 || arr[mid] != arr[mid+1])
-        return mid;
+        if (mid == n - 1 || arr[mid] != arr[mid + 1])
+            return mid;
         else
-        return fs(arr,mid+1,high,x,n);
-
+            return fs(arr, mid + 1, high, x, n);
     }
-    
 }
 
+// using iterative binary search
+int ss(int arr[], int n, int x)
+{
+    int low = 0, high = n - 1;
+    while (low < high)
+    {
+        int mid = (low + high) / 2;
+        if (arr[mid] < x)
+            low = mid + 1;
+        else if (arr[mid] > x)
+            high = mid - 1;
+
+        else
+        {
+            if (mid != n - 1 || arr[mid] != arr[mid + 1])
+                return mid;
+            else
+                low = mid + 1;
+        }
+    }
+    return -1;
+}
 int main()
 {
-    int arr[]={5,10,10,10,10,20,20};
-    cout<<fs(arr,0,6,10,7);
+    int arr[] = {5, 10, 10, 10, 10, 20, 20};
+    cout << fs(arr, 0, 6, 10, 7);
+    cout << ss(arr, 7, 10);
 }
