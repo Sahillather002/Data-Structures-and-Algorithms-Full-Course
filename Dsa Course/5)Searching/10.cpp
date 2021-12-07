efa35037068b13a7c3174c70e8a9be4d13972592
sahil lather <sahillather002@gmail.com>
2021-12-07 23:51:41 +0530

    peak element using binary search

--- Dsa Course/5)Searching/10.cpp
+++ Dsa Course/5)Searching/10.cpp
@@ -1,24 +1,47 @@
 // finding peak element in an array
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 // sol 1
-
-int fs(int arr[],int n)
+// O(n)
+int fs(int arr[], int n)
 {
-    if(n==1)
-    return arr[0];
-    if(arr[0]>=arr[1])
-    return arr[0];
-    if(arr[n-1]>=arr[n-2])
-    return arr[n-1];
-    for(int i=1;i<n-1;i++)
-    if(arr[i]>=arr[i-1] && arr[i]<=arr[i+1])
-    return arr[i];
+    if (n == 1)
+        return arr[0];
+    if (arr[0] >= arr[1])
+        return arr[0];
+    if (arr[n - 1] >= arr[n - 2])
+        return arr[n - 1];
+    for (int i = 1; i < n - 1; i++)
+        if (arr[i] >= arr[i - 1] && arr[i] <= arr[i + 1])
+            return arr[i];
+
+    return -1;      
 }
 
+// sol 2
+// log(n) -- time complexity
+
+int ss(int arr[],int n)
+{
+    int low = 0;
+    int high = n-1;
+    while(low<=high)
+    {
+        int mid = (low + high)/2;
+        if((mid == 0 || arr[mid-1]<=arr[mid]) &&
+        (mid == n-1 || arr[mid+1]<=arr[mid]))
+        return mid;
+        if(mid>0 && arr[mid-1]>=arr[mid])
+        high = mid-1;
+        else
+        low = mid+1;
+    }
+    return -1;
+}
 int main()
 {
-    int arr[]={ 10,7,8,20,12};
-    cout<<fs(arr,5);
+    int arr[] = {10, 7, 8, 20, 12};
+    cout << fs(arr, 5);
+    cout<<ss(arr,5);
 }
