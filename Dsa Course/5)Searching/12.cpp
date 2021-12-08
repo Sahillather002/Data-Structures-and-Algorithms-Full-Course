cd13da6e464ac3a6c7e4134dba2b17ee58bdb647
sahil lather <sahillather002@gmail.com>
2021-12-08 13:14:37 +0530

    definig median of two sorted array

--- Dsa Course/5)Searching/12.cpp
+++ Dsa Course/5)Searching/12.cpp
@@ -15,13 +15,20 @@ int fs(int arr[],int n,int x)
             for(int k=j+1;k<n;k++)
             {
                 if(arr[i]+arr[j]+arr[k]==x)
-                cout<<"Triplet is present";
+                cout<<"Triplet is present"<<endl;
             }
         }
     }
     return -1;
 }
+// sol 2
 
+int ss(int arr[],int x,int n)
+{
+    for(int i=0;i<n;i++)
+    if(ss(arr,i+1,n-1,x-a[i]))
+    return true;
+}
 int main()
 {
     int arr[]={2,3,4,8,9,20,40};
