55d02fd707cc2e3814d7aac75fd2ebd9bb55ffac
sahil lather <sahillather002@gmail.com>
2021-12-08 22:55:16 +0530

    more's voting algorithm

--- Dsa Course/5)Searching/14.cpp
+++ Dsa Course/5)Searching/14.cpp
@@ -18,8 +18,37 @@ int fs(int arr[], int n)
     return -1;
 }
 
+// More's Voting Algorithm
+int ss(int arr[],int n)
+{
+    int res = 0;
+    int count = 1;
+    for(int i=1;i<n;i++)
+    {
+        if(arr[res]==arr[i])
+        count++;
+        else
+        count--;
+        if(count==0)
+        {
+            res=i;
+            count = 1;
+        }
+    }
+
+    count =0;
+    for(int i=0;i<n;i++)
+    if(arr[res]==arr[i])
+    count++;
+    if(count<=n/2)
+    res = -1;
+
+    return res;
+}
+
 int main()
 {
     int arr[]={8,7,6,8,6,6,6,6};
     cout<<fs(arr,8);
+    cout<<ss(arr,8);
 }
