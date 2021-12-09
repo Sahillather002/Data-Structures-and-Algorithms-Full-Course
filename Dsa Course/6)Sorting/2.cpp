9dfaa161550f61c746932ea47c8ea8e9fc66b187
sahil lather <sahillather002@gmail.com>
2021-12-09 23:22:17 +0530

    vector sort

---
+++ Dsa Course/6)Sorting/2.cpp
@@ -0,0 +1,17 @@
+// sort in c++ stl
+// vector
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main()
+{
+    vector<int> v = {5, 7, 20, 10};
+    sort(v.begin(), v.end());
+    for (int x : v)
+        cout << x << " ";
+    cout << endl;
+   // sort(v.begin(), v.end(), greater<int>);
+    for (int x : v)
+        cout << x << " ";
+}
