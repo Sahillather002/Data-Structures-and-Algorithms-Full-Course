d840a77e64355dc3ab3e36e5b703526c32fccc0d
sahil lather <sahillather002@gmail.com>
2021-12-09 23:07:56 +0530

    sort stl array

---
+++ Dsa Course/6)Sorting/1.cpp
@@ -0,0 +1,16 @@
+// sort in c++ stl
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main()
+{
+    int arr[]={10,20,5,7};
+    int n=4;
+    sort(arr,arr+n);
+    for(int x:arr)
+    cout<<x<<" ";
+   // sort(arr,arr+n,greater<int>);
+    cout<<endl;
+    for(int x:arr)
+    cout<<x<<" ";
+}
