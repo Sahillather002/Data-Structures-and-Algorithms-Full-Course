27dcb3f7f4573f1c5c150596d5a83fe95366c479
sahil lather <sahillather002@gmail.com>
2021-12-10 23:24:17 +0530

    selection sort doc update

--- Dsa Course/6)Sorting/8.cpp
+++ Dsa Course/6)Sorting/8.cpp
@@ -41,3 +41,53 @@ int findSmallest(int myarray[], int i)
     }
     return position;
 }
+/*
+Introduction
+Selection sort is quite a straightforward sorting technique as the technique only involves finding the smallest element in every pass and placing it in the correct position.
+
+Selection sort works efficiently when the list to be sorted is of small size but its performance is affected badly as the list to be sorted grows in size.
+
+Hence we can say that selection sort is not advisable for larger lists of data.
+
+General Algorithm
+The General Algorithm for selection sort is given below:
+
+Selection Sort (A, N)
+
+Step 1: Repeat Steps 2 and 3 for K = 1 to N-1
+              Step 2: Call routine smallest(A, K, N,POS)
+              Step 3: Swap A[K] with A [POS]
+[End of loop]
+Step 4: EXIT
+
+Routine smallest (A, K, N, POS)
+
+Step 1: [initialize] set smallestElem = A[K]
+Step 2: [initialize] set POS = K
+Step 3: for J = K+1 to N -1,repeat
+if smallestElem > A [J]
+set smallestElem = A [J]
+set POS = J
+[if end]
+[End of loop]
+Step 4: return POS
+Pseudocode For Selection Sort
+Procedure selection_sort(array,N)
+    array – array of items to be sorted
+    N – size of array
+begin
+    for I = 1 to N-1
+    begin
+        set min  = i
+        for j = i+1 to N
+        begin
+            if array[j] < array[min] then
+                min = j;
+            end if
+        end for
+        //swap the minimum element with current element
+        if minIndex != I then
+            swap array[min[] and array[i]
+        end if
+    end for
+end procedure
