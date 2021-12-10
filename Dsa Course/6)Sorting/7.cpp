66eb022654bb35418f9dad5edc66d9a8836c4c96
sahil lather <sahillather002@gmail.com>
2021-12-11 00:07:47 +0530

    sol 1 selection sort

--- Dsa Course/6)Sorting/7.cpp
+++ Dsa Course/6)Sorting/7.cpp
@@ -9,3 +9,21 @@ quicksort,mergesort,insertion sort etc.
 */
 #include<iostream>
 using namespace std;
+
+//sol 1
+void fs(int arr[],int n)
+{
+    int temp[n];
+    for(int i=0;i<n;i++)
+    {
+        int min=0;
+        for(int j=1;j<n;j++)
+        if(arr[j]<arr[min])
+        min = j;
+        temp[i]=arr[min];
+        arr[min]= 0;
+
+    }
+    for(int i=0;i<n;i++)
+    arr[i]=temp[i];
+}
