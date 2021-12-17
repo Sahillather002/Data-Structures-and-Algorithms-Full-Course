7474379e841790c7f880d5bdc332b6f26583841d
sahil lather <sahillather002@gmail.com>
2021-12-17 19:28:28 +0530

    merge sort defining optimal solution

--- Dsa Course/6)Sorting/11.cpp
+++ Dsa Course/6)Sorting/11.cpp
@@ -1,4 +1,4 @@
-/* MERGE SORT 
+/* MERGE SORT
   it is a DAC (Divide and conquer) algo
   it is stable algo
   Theta (n * log(n))
@@ -7,26 +7,25 @@
   well suited for linked list , works in O(1)
   But , in general for arrays , quicksort outperforms
 */
-#include<iostream>
+#include <iostream>
 using namespace std;
 // merging two sorted array
 
-void merge1(int a[],int b[],int m,int n)
+void merge1(int a[], int b[], int m, int n)
 {
-    int c[m+n];  
-    for(int i=0;i<m;i++)
-    c[i]=a[i];
-    for(int i=0;i<n;i++)
-    c[m+i]=b[i];
-    sort(c,c+m+n);
-    for(int i=0;i<(m+n);i++)
-    cout<<c[i]<<" ";
+  int c[m + n];
+  for (int i = 0; i < m; i++)
+    c[i] = a[i];
+  for (int i = 0; i < n; i++)
+    c[m + i] = b[i];
+  sort(c, c + m + n);
+  for (int i = 0; i < (m + n); i++)
+    cout << c[i] << " ";
 }
 
 int main()
 {
-    int a[]={10,15,20,20};
-    int b[]={1,12};
-    merge1(a,b,4,2);
-
+  int a[] = {10, 15, 20, 20};
+  int b[] = {1, 12};
+  merge1(a, b, 4, 2);
 }
