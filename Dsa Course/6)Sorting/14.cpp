c6f2e5eac3344d6732c951a3169aa4334dd36c13
sahil lather <sahillather002@gmail.com>
2021-12-20 12:44:12 +0530

    merge function update

--- Dsa Course/6)Sorting/14.cpp
+++ Dsa Course/6)Sorting/14.cpp
@@ -2,34 +2,54 @@
 #include<iostream>
 using namespace std;
 
-void merge(int a[], int b[], int m, int n)
+void merge(int a[], int low, int mid, int high)
 {
-    int i = 0;
-    int j = 0;
-    while (i < m && j < n)
+    // deriving the seprate arrays
+    int n1 = mid - low + 1;
+    int n2 = high - mid;
+    int left[n1];
+    int right[n2];
+    for (int i = 0; i < n1; i++)
     {
-        if (a[i] <= b[i])
+        left[i] = a[low + i];
+    }
+    for (int i = 0; i < n2; i++)
+    {
+        right[i] = a[n1 + i];
+    }
+
+    // merging the array with sorting
+    int i = 0, j = 0, k = 0;
+    while (i < n1 && j < n2)
+    {
+        if (left[i] <= right[j])
         {
-            cout << a[i] << " ";
+            a[k] = left[i];
             i++;
+            k++;
         }
         else
         {
-            cout << b[i] << " ";
+            a[k] = right[i];
+            k++;
             j++;
         }
-        while (i < m)
+        // updating the values
+        while (i < n1)
         {
-            cout << a[i] << " ";
+            a[k] = left[i];
             i++;
+            k++;
         }
-        while (j < n)
+        while (j < n2)
         {
-            cout << b[j] << " ";
+            a[k] = right[j];
             j++;
+            k++;
         }
     }
 }
+
 void mergeSort(int arr[],int l,int r)
 {
     if(r>l) // checking for array having atleast two elements
