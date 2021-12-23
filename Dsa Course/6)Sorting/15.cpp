31536bb82afee84c209bf806154310d5161aef27
sahil lather <sahillather002@gmail.com>
2021-12-23 11:40:04 +0530

    intersection in two arrays

---
+++ Dsa Course/6)Sorting/15.cpp
@@ -0,0 +1,27 @@
+// intersection of two sorted array
+#include<iostream>
+using namespace std;
+
+void fs(int a[],int b[],int m,int n)
+{
+    for(int i=0;i<m;i++)
+    {
+        if(i>0 && a[i]==a[i-1])
+        continue;
+        for(int j=0;j<n;j++)
+        {
+            if(a[i]==b[j])
+            {
+                cout<<a[i]<< " ";
+                break;
+            }
+        }
+    }
+}
+
+int main()
+{
+    int a[]={1,20,20,40,60};
+    int b[]={2,20,20,20};
+    fs(a,b,5,4);
+}
