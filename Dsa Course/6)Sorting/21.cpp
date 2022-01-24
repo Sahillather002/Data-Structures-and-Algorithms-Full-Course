54722e724c063a8eb8eefd044f3d6ee07ecb3b34
sahil lather <sahillather002@gmail.com>
2022-01-24 15:10:06 +0530

    partition funtion

--- Dsa Course/6)Sorting/21.cpp
+++ Dsa Course/6)Sorting/21.cpp
@@ -6,7 +6,7 @@ using namespace std;
 void fs(int arr[], int l , int h , int p)
 {
     int temp[h-l+1] , index=0;
-    for(int i=l,i<=h;i++)
+    for(int i=l;i<=h;i++)
     if(arr[i] <= arr[p])
     {
         temp[index]=arr[i];
@@ -20,4 +20,14 @@ void fs(int arr[], int l , int h , int p)
     }
     for(int i=l;i<=h;i++)
     arr[i] = temp[i-l];
+}
+
+int main()
+{
+    int arr[]={5,13,6,9,12,11,8};
+    fs(arr,0,6,6);
+    for(int i=0;i<6;i++)
+    {
+        cout<<arr[i];
+    }
 }
