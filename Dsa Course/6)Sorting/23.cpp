c7feb74852f3db2e62b6ac61b11231d3737a8384
sahil lather <sahillather002@gmail.com>
2022-02-20 18:27:31 +0530

    file update

--- Dsa Course/6)Sorting/23.cpp
+++ Dsa Course/6)Sorting/23.cpp
@@ -1,25 +1,31 @@
-//Hoare's Partition
-#include<iostream>
+// Hoare's Partition
+#include <iostream>
 using namespace std;
-int partition(int arr[], int l,int h){
-    int pivot=arr[l];
-    int i=l-1;
-    int j=h-1;
-    while(true)
+int partition(int arr[], int l, int h)
+{
+    int pivot = arr[l];
+    int i = l - 1;
+    int j = h - 1;
+    while (true)
     {
-        do{
+        do
+        {
             i++;
-        }while(arr[i]<pivot);
-        do{
+        } while (arr[i] < pivot);
+        do
+        {
             j--;
-        }while(arr[j]>pivot);
+        } while (arr[j] > pivot);
+        if (i >= j)
+            return j;
+        swap(arr[i], arr[j]);
     }
-    if(i>=j)
-    return j;
-    swap(arr[i],arr[j]);
 }
-int main(){
-    int arr[]={5,3,8,4,2,7,1,10};
-    partition(arr,0,7);
-    cout<<arr[0];
+int main()
+{
+    int arr[] = {5, 3, 8, 4, 2, 7, 1, 10};
+    partition(arr, 0, 7);
+    cout << arr[0]<<endl;
+    for(int i=0;i<7;i++)
+    cout<<arr[i]<<" ";
 }
