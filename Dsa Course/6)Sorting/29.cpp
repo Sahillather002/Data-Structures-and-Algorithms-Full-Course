e8d47a32dcdba350aec2175c7e59529de1e95486
sahil lather <sahillather002@gmail.com>
2022-02-20 19:39:03 +0530

    kth smallest using lomuto partition

---
+++ Dsa Course/6)Sorting/29.cpp
@@ -0,0 +1,41 @@
+//k-th smallest element
+//using lomuto partition
+#include<iostream>
+using namespace std;
+
+int partition(int arr[], int l, int h)
+{
+    int pivot = arr[h];
+    int i = l - 1;
+    for (int j = l; j <= h - 1; j++)
+    {
+        if (arr[j] < pivot)
+        {
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+    swap(arr[i + 1], arr[h]);
+    return (i + 1);
+}
+
+int kSmall(int arr[],int n,int k){
+    int l=0;
+    int r=n-1;
+    while(l<=r)
+    {
+        int p = partition(arr,l,r);
+        if(p==k-1)
+        return p;
+        else if(p>k-1)
+        r=p-1;
+        else
+        l=p+1;
+    }
+    return -1;
+}
+int main(){
+    int arr[]={10,4,5,8,11,6,26};
+    cout<<kSmall(arr,7,5);
+    
+}
