82ba7dcca755ef4962d1c842f2897fd27bf94231
sahil lather <sahillather002@gmail.com>
2022-02-20 18:41:02 +0530

    keys about quick sort algo

---
+++ Dsa Course/6)Sorting/24.cpp
@@ -0,0 +1,16 @@
+// QUICK SORT ALGORITHM
+// Introducing quick sort algorithm
+/*
+--it is an divide and conquer algorithm
+--worst case time=O(n^2)
+--but still better then merge sort
+--it is inplace algo
+--it is cache friendly
+--average case is O(nlogn)
+--it is tail recursive algo
+--partition is key functions:
+1.naive - stable 
+2.lomuto  
+3.hoare 
+-- for stablity Merge Sort is Preferred
+*/
