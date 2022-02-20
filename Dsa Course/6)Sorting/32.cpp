6af99df612d29ae5551c03e5353d3a3ec5bd911e
sahil lather <sahillather002@gmail.com>
2022-02-20 20:46:09 +0530

    file update

--- Dsa Course/6)Sorting/32.cpp
+++ Dsa Course/6)Sorting/32.cpp
@@ -3,3 +3,35 @@
 // using hoare's partition
 #include<iostream>
 using namespace std;
+
+void segPosNeg(int arr[],int n)
+{
+    int i=-1;
+    int j=n;
+    while(true)
+    {
+        do
+        {
+            i++;
+        }while(arr[i]<0);
+
+        do
+        {
+            /* code */
+            j--;
+        } while (arr[j]>0);
+        
+        if(i>=j)
+        return;
+        swap(arr[i],arr[j]);
+    }
+}
+
+int main()
+{
+    int arr[] = {-12,18,-10,15};
+    segPosNeg(arr,4);
+    for(int i=0;i<4;i++)
+    cout<<arr[i]<<endl;
+    
+}
