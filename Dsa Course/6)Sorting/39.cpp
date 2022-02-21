9c392658e0f7bb4ac0c3b8a054b28a276a9bcc04
sahil lather <sahillather002@gmail.com>
2022-02-21 23:12:35 +0530

    count sort naive sol

--- Dsa Course/6)Sorting/39.cpp
+++ Dsa Course/6)Sorting/39.cpp
@@ -4,4 +4,34 @@ uses when small inputs are there
 complexity=O(n+k)
 to sort n element in range from
 o to k-1;
+It is useful only when k is linear
 */
+#include<iostream>
+using namespace std;
+//naive sol
+void countSort(int arr[],int n,int k)
+{
+    int count[k];
+    for(int i=0;i<k;i++)
+    count[i]=0;
+    for(int i=0;i<n;i++)
+    count[arr[i]]++;
+
+    int index = 0;
+    for(int i=0;i<k;i++)
+    {
+        for(int j=0;j<count[i];j++)
+        {
+            arr[index]=i;
+            index++;
+        }
+    }
+}
+
+int main()
+{
+    int arr[]={1,4,4,1,0,1};
+    countSort(arr, 6, 5);
+    for(int i=0;i<6;i++)
+    cout<<arr[i]<<" ";
+}
