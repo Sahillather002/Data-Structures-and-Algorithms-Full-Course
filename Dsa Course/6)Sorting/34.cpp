4dd0629feab5dd09df64800d450f264928b8b710
sahil lather <sahillather002@gmail.com>
2022-02-21 18:54:41 +0530

    def.sort for dnf algo :

--- Dsa Course/6)Sorting/34.cpp
+++ Dsa Course/6)Sorting/34.cpp
@@ -13,3 +13,26 @@ further about mid is below
 */
 #include<iostream>
 using namespace std;
+
+void sort(int arr[],int n)
+{
+    int low = 0;
+    int high = n-1;
+    int mid = 0;
+    while(mid <= high)
+    {
+        if(arr[mid]==0)
+        {
+            swap(arr[low],arr[mid]);
+            low++;
+            mid++;
+        }
+        else if(arr[mid]==1)
+        mid ++;
+        else 
+        {
+            swap(arr[mid],arr[high]);
+            high--;
+        }
+    }
+}
