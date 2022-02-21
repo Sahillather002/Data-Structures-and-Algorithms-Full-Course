892608dc1b7df5a5f4521a06a3f8bbc24842fc50
sahil lather <sahillather002@gmail.com>
2022-02-21 18:57:11 +0530

    dnf complete

--- Dsa Course/6)Sorting/34.cpp
+++ Dsa Course/6)Sorting/34.cpp
@@ -1,38 +1,46 @@
 // sort an array with tree types
 // better approach
 // Dutch National Flag Algorithm
-//using hoare's algo
+// using hoare's algo
 /* -- explaining dnf algo --
 we gona maintain four sections in our array
-first is of 0's 
+first is of 0's
 second is of 1's
-3rd is of unkonown's 
+3rd is of unkonown's
 and 4th is of 2's
 --using two pointers low and high
 further about mid is below
 */
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-void sort(int arr[],int n)
+void sort(int arr[], int n)
 {
     int low = 0;
-    int high = n-1;
+    int high = n - 1;
     int mid = 0;
-    while(mid <= high)
+    while (mid <= high)
     {
-        if(arr[mid]==0)
+        if (arr[mid] == 0)
         {
-            swap(arr[low],arr[mid]);
+            swap(arr[low], arr[mid]);
             low++;
             mid++;
         }
-        else if(arr[mid]==1)
-        mid ++;
-        else 
+        else if (arr[mid] == 1)
+            mid++;
+        else
         {
-            swap(arr[mid],arr[high]);
+            swap(arr[mid], arr[high]);
             high--;
         }
     }
+}
+
+int main()
+{
+    int arr[]={0,1,2,1,1,2};
+    sort(arr,6);
+    for(int i=0;i<6;i++)
+    cout<<arr[i]<<" ";
 }
