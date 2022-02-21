d86195fad47aa83ce344059f59667c54abe88a83
sahil lather <sahillather002@gmail.com>
2022-02-21 22:58:40 +0530

    defining heafiy

--- Dsa Course/6)Sorting/38.cpp
+++ Dsa Course/6)Sorting/38.cpp
@@ -1,6 +1,8 @@
 // heap sort
-#include <iostream>
+// worst case complexity is nlogn
+#include <bits/stdc++.h>
 using namespace std;
+// STEP - 1
 // builiding a max heap
 void maxHeapify(int arr[], int n, int i)
 {
@@ -25,4 +27,19 @@ void buildHeap(int arr[], int n)
 {
     for (int i = (n - 2) / 2; i >= 0; i--)
         maxHeapify(arr, n, i);
+}
+
+// STEP-2
+/*
+repeatedly swap root with last node,
+reduce heap size by one and heapify
+*/
+void heapSort(int arr[],int n)
+{
+    buildHeap(arr,n);
+    for(int i=n-1;i>=1;i--)
+    {
+        swap(arr[0],arr[i]);
+        heapify(arr,i,0);
+    }
 }
