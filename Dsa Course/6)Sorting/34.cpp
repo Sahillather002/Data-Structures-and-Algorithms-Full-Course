172dc40ae2e01258f679fdc80c3c5621aacbc9c3
sahil lather <sahillather002@gmail.com>
2022-02-21 18:52:34 +0530

    dutch national flag algorithm explanation

---
+++ Dsa Course/6)Sorting/34.cpp
@@ -0,0 +1,15 @@
+// sort an array with tree types
+// better approach
+// Dutch National Flag Algorithm
+//using hoare's algo
+/* -- explaining dnf algo --
+we gona maintain four sections in our array
+first is of 0's 
+second is of 1's
+3rd is of unkonown's 
+and 4th is of 2's
+--using two pointers low and high
+further about mid is below
+*/
+#include<iostream>
+using namespace std;
