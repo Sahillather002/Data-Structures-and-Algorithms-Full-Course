9f8ddb544d11d30e80f8941133addfed789529d3
sahil lather <sahillather002@gmail.com>
2022-02-21 15:39:06 +0530

    file update

--- Dsa Course/6)Sorting/33.cpp
+++ Dsa Course/6)Sorting/33.cpp
@@ -1,4 +1,35 @@
 //sort an array with three types
 #include<iostream>
 using namespace std;
-
+//naive solution
+void sort(int arr[],int n)
+{
+    int temp[n],i=0;
+    for(int j=0;j<n;j++)
+    if(arr[j]==0)
+    {
+        temp[i]=arr[j];
+        i++;
+    }
+    for(int j=0;j<n;j++)
+    if(arr[j]==1)
+    {
+        temp[i]=arr[j];
+        i++;
+    }
+    for(int j=0;j<n;j++)
+    if(arr[j]==2)
+    {
+        temp[i]=arr[j];
+        i++;
+    }
+    for(int j=0;j<n;j++)
+    arr[j]=temp[j];
+}
+int main()
+{
+    int arr[]={0,1,1,2,0,1};
+    sort(arr,6);
+    for(int i=0;i<6;i++)
+    cout<<arr[i];
+}
