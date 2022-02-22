1155417817df23cc84568ce079908ea194f2fccb
sahil lather <sahillather002@gmail.com>
2022-02-22 10:38:42 +0530

    chaining in hashing

--- Dsa Course/8)Hashing/1.cpp
+++ Dsa Course/8)Hashing/1.cpp
@@ -1,3 +1,35 @@
 // Implementation of chaining
-#include<iostream>
+#include <bits/stdc++.h>
 using namespace std;
+// bucket size is already provided
+
+struct myHash
+{
+    int bucket;
+    list<int> *table; // used for implementing doubly linked list
+
+    myHash(int b)
+    {
+        bucket = b;
+        table = new list<int>[b];
+    }
+
+    void insert(int key)
+    {
+        int i = key % bucket;
+        table[i].push_back(key);
+    }
+    void remove(int key)
+    {
+        int i = key % bucket;
+        table[i].remove(key);
+    }
+    bool search(int key)
+    {
+        int i = key % bucket;
+        for (auto x : table[i])
+            if (x == key)
+                return true;
+        return false;
+    }
+}
