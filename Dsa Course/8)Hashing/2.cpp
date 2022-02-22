a95b19e113fca3c465102aafa6b41757051b41cb
sahil lather <sahillather002@gmail.com>
2022-02-22 13:41:16 +0530

    hash code

--- Dsa Course/8)Hashing/2.cpp
+++ Dsa Course/8)Hashing/2.cpp
@@ -1,3 +1,23 @@
 // Implementation of open addressing
 #include<bits/stdc++.h>
 using namespace std;
+// -1 represent a free slot
+// -2 represent a deleted slot
+// assuming user provide the input hash size
+
+struct myHash
+{
+    int *arr;
+    int cap ,size;
+    myHash(int c)
+    {
+        cap = c;
+        size = 0;
+        for(int i=0;i<cap;i++)
+        arr[i] = -1;
+    }
+    int hash(int key)
+    {
+        return key % cap;
+    }
+}
