6a9b27fffc87c1bb02d9013806ff4637ab004d35
sahil lather <sahillather002@gmail.com>
2022-02-23 08:52:30 +0530

    unordered map in c++ stl

--- Dsa Course/8)Hashing/8.cpp
+++ Dsa Course/8)Hashing/8.cpp
@@ -7,3 +7,16 @@ but unordered map is based on hashing
 - there is no order of keys
 - search , insert , delete are of O(1)
 */
+#include<iostream>
+#include<unordered_map> 
+using namespace std;
+int main()
+{
+    unordered_map<string, int> m;
+    m["abc"] = 20;
+    m["def"] = 30;
+    m.insert({"ghi", 15});
+    for(auto x : m)
+    cout<<x.first<<" "<<x.second<<endl;
+    return 0;
+}
