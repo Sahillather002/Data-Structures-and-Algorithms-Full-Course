3b4e5fc60223db557658b12ad1801a317ab245ef
sahil lather <sahillather002@gmail.com>
2022-02-23 08:49:49 +0530

    unordered map def

---
+++ Dsa Course/8)Hashing/8.cpp
@@ -0,0 +1,9 @@
+// unordered map in c++ stl
+/*
+- it is used to store key , value pair
+- ordinarly map is based on red black tree
+but unordered map is based on hashing
+- it uses hashing
+- there is no order of keys
+- search , insert , delete are of O(1)
+*/
