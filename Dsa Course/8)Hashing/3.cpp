a61a06a8af1e03a91d33197f7e46d49c6d785a36
sahil lather <sahillather002@gmail.com>
2022-02-23 08:27:28 +0530

    unordered set in c++ stl

---
+++ Dsa Course/8)Hashing/3.cpp
@@ -0,0 +1,16 @@
+// unordered set in c++ stl
+#include<iostream>
+#include<unordered_set>
+using namespace std;
+int main()
+{
+    unordered_set<int> s;
+    s.insert(10);
+    s.insert(5);
+    s.insert(15);
+    s.insert(20);
+
+    for(int x : s)
+    cout<<x<<" ";
+    return 0;
+}
